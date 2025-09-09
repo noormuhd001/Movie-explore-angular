fadc1ac9db48aba73e7b37090ebaa024f63f59cd
Noor muhammed <noormuhammed@bytekat.com>
2025-09-09 10:30:55 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'testing';
+
+  testForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+  })
+  submit() {
+    console.log(this.testForm.value);
+  }
+
+  get username() {
+    return this.testForm.get('username');
+  }
+
+  get password() {
+    return this.testForm.get('password');
+  }
+
 }
